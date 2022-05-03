9b0fd54af71fab7339f3bb58564387a365bc7545
William Lieurance <william.lieurance@namikoda.com>
2022-05-03 16:49:44 -0500

    Working minimal error example

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import { FileUpload } from 'primereact/fileupload';
+
+const uploadURL = "https://primefaces.org/fakeimageupload";
+
+const onUpload = () => {
+  //Doesn't even get this far
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <FileUpload name="images"
+          url={uploadURL}
+          onUpload={onUpload}
+          accept="image/*"
+          chooseLabel="Upload Pictures"
+          emptyTemplate={<p className="p-m-0">Drag and drop images to here to upload.</p>}
+      />
     </div>
   );
 }
